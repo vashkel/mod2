85805f98a02dd11b34d0d000913601c7d7fa3572
Vashkevich Yura <nonstop_blackfoxx@mail.ru>
2021-01-19 19:24:55 +0300

    some fix yet

--- product/src/main/java/com/epam/esm/GiftCertificateApplication.java
+++ product/src/main/java/com/epam/esm/GiftCertificateApplication.java
@@ -3,6 +3,9 @@ package com.epam.esm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class GiftCertificateApplication {
 
@@ -10,4 +13,9 @@ public class GiftCertificateApplication {
         SpringApplication.run(GiftCertificateApplication.class, args);
     }
 
+    @PostConstruct
+    public void init(){
+        // Setting Spring Boot SetTimeZone
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 }
